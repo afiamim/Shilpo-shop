ab064a06c15e46096a972cf4e506a276edad7868
siammubc12-a11y <siammubc12@gmail.com>
2026-05-14 05:12:57 +0600

    update

--- reviews_app/urls.py
+++ reviews_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add/<int:product_id>/',views.add_review,name='add_review'),
     path('product/<int:product_id>/',views.review_list,name='review_list'),
     path('delete/<int:id>/',views.delete_review,name='delete_review'),
+]
 
