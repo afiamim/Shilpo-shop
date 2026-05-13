2b6548bced0b82ca078f78bc11b45431ceae24e4
siammubc12-a11y <siammubc12@gmail.com>
2026-05-14 04:15:33 +0600

    update

--- Shilposhop/urls.py
+++ Shilposhop/urls.py
@@ -19,16 +19,16 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('products/', include('products_app.urls')),
-    path('payments/', include('payments_app.urls')),
-    path('orders/', include('orders_app.urls')),
-=======
 
     path('products/', include('products_app.urls')),
     path('payments/', include('payments_app.urls')),
     path('orders/', include('orders_app.urls')),
 
->>>>>>> 14027be4848c782c6e76b3b4189e7eaff589bdf1
+
+    path('products/', include('products_app.urls')),
+    path('payments/', include('payments_app.urls')),
+    path('orders/', include('orders_app.urls')),
+
+
 
 ]
